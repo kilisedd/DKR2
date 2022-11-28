b714dc7451d2139562ee7bf41a24ca1b69d15e0e
kilisedd <117628693+kilisedd@users.noreply.github.com>
2022-11-28 17:32:22 +0300

    Add files via upload

---
+++ zad1.pas
@@ -0,0 +1,16 @@
+﻿const tat = 11;
+var a:array of integer;k:byte;
+begin
+  SetLength(a,tat);
+  for var i:= 1 to 10 do
+  begin
+    print('a[',i,'] = ');
+    a[i]:=readinteger;
+  end;
+  println;
+  for var i:= 2 to 10 do
+    if a[i-1]>=a[i] then inc(k);
+    if k = 0 then println('массив возрастающий')
+    else
+      println ('массив не возрастающий');
+end.
---
+++ zad2.pas
@@ -0,0 +1,16 @@
+﻿var str:string;count:integer;
+begin
+  str:=readstring;
+  var len:= length(str);
+  count:=0;
+ for var i:=1 to len do 
+  if str[i]= ' ' then
+      count:=count + 1;
+   if len>0 then
+     count:= count + 1;
+   println('кол-во слов: ',count);
+   count:=0;
+   for var i:=1 to len do
+     count:=count+1;
+   println('кол-во символов: ',count);
+end.
